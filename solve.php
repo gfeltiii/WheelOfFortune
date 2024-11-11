8a0524d9bad9d9452e118ca4a094d1999125c991
gfeltiii <gfeltoniii@gmail.com>
2024-11-10 22:05:22 -0500

    finishing touches

--- solve.php
+++ solve.php
@@ -10,16 +10,13 @@ if (isset($_SESSION["gameMoney1"])==false){
   $_SESSION["gameMoney1"]= $_SESSION["gameMoney2"]= $_SESSION["gameMoney3"]= $_SESSION["roundMoney1"]=$_SESSION["roundMoney2"]= $_SESSION["roundMoney3"]=0;
 }
 if (isset($_SESSION['players'])==false){
-    $_SESSION['players'] = array("p","e","m");
+    $_SESSION['players'] = array("p","p","p");
     $_SESSION['turn']=0;
 }
 if (isset($_SESSION["lettersGuessed"])==false){
     $_SESSION["lettersGuessed"]= array();
 }
 $consonants = array("q","j","z","x","v","k","w","y","f","b","g","h","m","p","d","c","l","s","n","t","r");
-
-$clue = array("s","t","r","a","w","b","e","r","r","i","e","s","|","a","n","d","_","c","r","e","a","m");
-$cluetext = "strawberries and cream";
 if ($_SESSION['players'][$_SESSION['turn']]=="e"){
   foreach ($consonants as $l){
     if (in_array($l,$_SESSION["lettersGuessed"])==false){
@@ -40,7 +37,7 @@ if (isset($_POST['guess']) && $_POST['guess']!='?'){
       $_SESSION["lettersGuessed"][] = $_POST['guess'];
     }
 }
-$right = (isset($_POST['solveGuess']) && strcasecmp($_POST['solveGuess'],$cluetext)== 0);
+$right = (isset($_POST['solveGuess']) && strcasecmp($_POST['solveGuess'],$_SESSION['cluetext'])== 0);
 if(isset($_POST['solveGuess'])){ 
 $_SESSION['display']=!$_SESSION['display'];
 }
@@ -54,7 +51,8 @@ $_SESSION['display']=!$_SESSION['display'];
 <body>
         <div class="topHalf">
             <div class="leftSidebar">
-                Spin Money: <?php print($_SESSION['spinMoney']."$"); ?><br><br>
+                Round: <?php print($_SESSION['round']);?><br><br>
+                Spin Money: <?php print($_SESSION['spinMoney']); ?><br><br>
                 Letters Guessed: <?php foreach($_SESSION['lettersGuessed'] as $key => $value) {
                  print(strtoupper($value)); 
                  if ($key!=count($_SESSION['lettersGuessed'])-1){
@@ -70,7 +68,7 @@ $_SESSION['display']=!$_SESSION['display'];
                  $r=53.63;
                  $t=34.75;
                  $j=0;
-                 foreach ($clue as $key => $value) {
+                 foreach ($_SESSION['clue'] as $key => $value) {
                      if ($value!="_" && $value!="|") {
                         if(in_array($value,$_SESSION["lettersGuessed"])){
                           if($value == $_POST['guess']){
@@ -98,11 +96,11 @@ $_SESSION['display']=!$_SESSION['display'];
                 ?>
             </div>
             <div class="rightSidebar">
-                Player 1: <?php print($_SESSION['roundMoney1']);?>$ <br>
-                Bank: <?php print($_SESSION['gameMoney1']);?>$ <br>
-                Player 2: <?php print($_SESSION['roundMoney2']);?>$ <br>
+            <?php if($_SESSION['turn']==0)print("&#8594;");?>Player 1: <?php print($_SESSION['roundMoney1']);?>$ <br>
+                Bank: <?php print($_SESSION['gameMoney1']);?>$ <br><br>
+                <?php if($_SESSION['turn']==1)print("&#8594;");?>Player 2: <?php print($_SESSION['roundMoney2']);?>$ <br><br>
                 Bank: <?php print($_SESSION['gameMoney2']);?>$ <br>
-                Player 3: <?php print($_SESSION['roundMoney3']);?>$ <br>
+                <?php if($_SESSION['turn']==2)print("&#8594;");?>Player 3: <?php print($_SESSION['roundMoney3']);?>$ <br><br>
                 Bank: <?php print($_SESSION['gameMoney3']);?>$ <br>
             </div>
         </div>
@@ -117,6 +115,7 @@ $_SESSION['display']=!$_SESSION['display'];
         <?php exit();}
         if($right){
             $money=0;
+            $_SESSION['clue']="";
             
             if ($_SESSION['turn']==0){
                 $_SESSION['gameMoney1']+=$_SESSION['roundMoney1'];
@@ -128,16 +127,31 @@ $_SESSION['display']=!$_SESSION['display'];
                 $_SESSION['gameMoney3']+=$_SESSION['roundMoney3'];
                 $money=$_SESSION['roundMoney3'];
             }
-           
+           if ($money< 1000){
+            $money=1000;
+          }
             $_SESSION['round']++;
+            print("<div class='bottomHalf'>");
             print('<h2>Player '. ($_SESSION["turn"]+1) .' Is Correct!<br>They Banked '. $money .'$ This Round</h2>');
-                $_SESSION['round']= ($_SESSION['round']+1)%3;
+                $_SESSION['round']= ($_SESSION['round']+1);
                 $_SESSION['roundMoney1']= $_SESSION['roundMoney2'] = $_SESSION['roundMoney3'] = 0;
                 $_SESSION['lettersGuessed']= array_diff($_SESSION['lettersGuessed'], $_SESSION['lettersGuessed']);
+            print("</div>");
+              if($_SESSION['round']>2){
+                print('<a href="./leaderboard.html">Results</a>');
+                $_SESSION['round']=0;
+              }else{
+                print('<a href="board.php">Continue</a>');
+              }
+            }
+            else{
+              $_SESSION['turn']=($_SESSION['turn']+1)%3;
+              print("<div class='bottomHalf'>");
+              print('<h2>Player '. ($_SESSION["turn"]) .' Is Wrong!<br>It is now Player '. ($_SESSION["turn"]) .'\'s Turn</h2>');
+              print("</div>");
+            }
 
-             
-            }?>
-            <a href="board.php">Continue</a>
+              ?>
         </body>
 
 
